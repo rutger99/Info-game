c22392acda406608b718d8c80916208d94c33ed5
victorschreuder <45063011+victorschreuder@users.noreply.github.com>
2019-03-27 02:26:02 +0100

    game over scherm en obstakels werken

---
+++ Info-The-Game/Assets/Scripts/collide.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class collide : MonoBehaviour
+{
+ 
+     void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.name == "player")
+        {
+        
+            SceneManager.LoadScene(0);
+                     }
+    }
+}
