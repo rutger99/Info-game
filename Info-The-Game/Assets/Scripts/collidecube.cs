df445d3f9da557eafab98504177933da2ba44958
victorschreuder <45063011+victorschreuder@users.noreply.github.com>
2019-04-04 01:39:39 +0200

    Punten in gui, spelrgels en meer

---
+++ Info-The-Game/Assets/Scripts/collidecube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class collidecube : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == "player")
+        {
+
+            SceneManager.LoadScene(3);
+        }
+    }
+}
---
+++ Info-The-Game/Assets/Scripts/puntenscript.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class puntenscript : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+
+
+    void Start()
+    {
+        startTime = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = (Time.time - startTime) * 15;
+
+        string punten1 = (t).ToString("f0");
+        
+
+        timerText.text = "Score" + ":" + " " + punten1;
+    }
+}
