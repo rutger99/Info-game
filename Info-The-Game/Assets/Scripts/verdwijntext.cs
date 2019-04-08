f89e0b8024b8798ffa656d8a482aefe14d634850
victorschreuder <45063011+victorschreuder@users.noreply.github.com>
2019-04-08 23:59:22 +0200

    klaar

---
+++ Info-The-Game/Assets/Scripts/verdwijntext.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+public class verdwijntext : MonoBehaviour
+{
+
+    public float stayTime = 2.0f;
+    public float fadeoutTime = 1.0f;
+
+    private float time;
+
+    public void Start()
+    {
+        time = stayTime + fadeoutTime;
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time < fadeoutTime && time > 0)
+        {
+            float alpha = time / fadeoutTime;
+            foreach (CanvasRenderer r in GetComponentsInChildren<CanvasRenderer>())
+            {
+                r.SetAlpha(alpha);
+            }
+        }
+
+        if (time <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Info-The-Game/Assets/Scripts/zwaartekracht.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zwaartekracht : MonoBehaviour
+{
+    void zwaartekrachten()
+    {
+        Physics.gravity = new Vector3(0, 100.0F, 0);
+    }
+}
